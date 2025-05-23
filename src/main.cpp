000d90e86d7dda6099fe7b713b307b150f034f0a
Zeneth <gr3goire.faure@gmail.com>
2025-05-23 14:54:16 +0200

    Particules statiques

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,14 @@
 #include "opengl-framework/opengl-framework.hpp"
 #include "utils.hpp"
 
+struct Particle {
+    glm::vec2 position;
+
+    Particle()
+        : position{utils::rand(-gl::window_aspect_ratio(), gl::window_aspect_ratio()),
+                   utils::rand(-1.f, 1.f)} {}
+};
+
 int main()
 {
     gl::init("Particules!");
@@ -8,14 +16,22 @@ int main()
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE);
 
-    // TODO: create an array of particles
+    // Vect de particule
+    std::vector<Particle> particles;
+
+    for (int i = 0; i < 100; ++i) {
+        particles.emplace_back();
+    }
 
     while (gl::window_is_open())
     {
         glClearColor(0.f, 0.f, 0.f, 1.f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-        // TODO update particles
-        // TODO render particles
+        // Render des particules
+        for (const auto& p : particles) {
+            utils::draw_disk(p.position, 0.02f, glm::vec4(1.f, 1.f, 1.f, 1.f));
+        }
+
     }
 }
