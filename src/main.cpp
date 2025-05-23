111c659ea2211e8fa933fe9ac1a5348b4c6b22c9
Zeneth <gr3goire.faure@gmail.com>
2025-05-23 15:04:12 +0200

    Force de gravité

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 struct Particle {
     glm::vec2 position;
     glm::vec2 velocity;
+    float mass;
 
     Particle()
     {
@@ -15,6 +16,7 @@ struct Particle {
         // random speed nd stats
         float angle = utils::rand(0.f, 2.f * 3.14159f); //environ pi
         float speed = utils::rand(0.2f, 0.6f);
+        mass = utils::rand(0.5f, 2.f); // rand between 0.5 et 2
 
         velocity = glm::vec2(std::cos(angle), std::sin(angle)) * speed;
     }
@@ -45,7 +47,13 @@ int main()
 
         // Update de la pos
         for (auto& p : particles) {
-            p.position += p.velocity * dt;
+
+            glm::vec2 gravity = glm::vec2(0.f, -1.00f * p.mass); 
+            glm::vec2 acceleration = gravity / p.mass;
+            p.velocity += acceleration * dt;
+
+            p.position += p.velocity * dt; // Update finale de la pos
+            
         }
 
         // Render des particules
